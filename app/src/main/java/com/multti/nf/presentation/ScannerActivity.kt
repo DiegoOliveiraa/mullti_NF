a9b067ea6d6ded8d3d6af37b6b60b3f2cd41cd09
Diego Oliveira <padawanGM@gmail.com>
2023-10-01 18:32:03 -0300

    Revert "Leitor de Código de Barras parte 1"
    This reverts commit 5231234a7fa0db548c4c746fbed91ef7b6ff71ed.

---
+++ app/src/main/java/com/multti/nf/presentation/ScannerActivity.kt
@@ -0,0 +1,12 @@
+package com.multti.nf.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.multti.nf.R
+
+class ScannerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scanner)
+    }
+}
